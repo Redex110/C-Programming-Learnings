18cae670a4b72da8abbfe40cad64c67718c1c201
RedeX110 <devendra.red.11@gmail.com>
2026-05-09 09:43:51 +0530

    Add postive and nagitive number counter

---
+++ Positive & Nagitive Number Counter/README.md
@@ -0,0 +1,19 @@
+# Positive & Nagitive Number Counter
+
+## Purpose
+This is a program that count number of positive, nagitive, and zero from the number that we input and display the result.
+## Input
+-
+
+## Output
+-
+
+## How to Compile
+```bash
+gcc main.c -o program
+```
+
+## How to Run
+```bash
+./program
+```
---
+++ Positive & Nagitive Number Counter/main.c
@@ -0,0 +1,56 @@
+/*
+--------------------------------------------------
+Program : Positive & Nagitive Number Counter
+Purpose : This program count the number of positive, nagitive and zeros number.
+
+Author  : Gray Storm
+Date    : 03 May 2026
+
+Input   : 10 Number e.g. {1, 2, -2, 3, 2, 0, 7, -7, 9, 0}
+Output  : Number of Positive Numbers : 6
+	  Number of Nagitive Numbers : 2
+	  Number of Zeros            : 2
+--------------------------------------------------
+*/
+
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void)
+{
+	//The variable delcaration
+	int number[SIZE], i, positive_number = 0, nagitive_number = 0, zero_numbers = 0;
+
+	//The Input Loop
+	for(i = 0;i < SIZE; i++)
+	{
+
+		printf("Enter number[%d] : ", i);
+		scanf("%d", &number[i]);
+	}
+
+	//The Counter Loop
+	for(i = 0; i < SIZE; i++)
+	{
+		if(number[i] < 0)
+		{
+			nagitive_number++;
+		}
+		else if(number[i] > 0)
+		{
+			positive_number++;
+		}
+		else
+		{
+			zero_numbers++;
+		}
+	}
+
+	//Output
+	printf("Number of Positive Number : %d\n", positive_number);
+	printf("Number of Nagitive Number : %d\n", nagitive_number);
+	printf("Number of Zeros           : %d\n", zero_numbers);
+
+	return 0;
+}
